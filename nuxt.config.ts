0b377c88371fc31f226bb1c701df0545cef34642
michalcecko <ceckomichal@gmail.com>
2025-03-23 18:00:26 +0100

    Add custom fonts, global styles, and Tailwind extensions
    Integrated Inter variable font, defined global typography and colors in Tailwind, and customized heading styles. Updated Nuxt configuration to include new CSS files and extended Tailwind for better design consistency. Removed placeholder content from index.vue.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
+    compatibilityDate: '2025-03-23',
     devtools: {enabled: true},
     modules: [
         '@nuxt/eslint',
         '@nuxtjs/tailwindcss',
+    ],
+    css: [
+        '~/assets/css/fonts.css',
+        '~/assets/css/global.css',
     ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     theme: {
-       //
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+            },
+            colors: {
+                background: '#0F0F0F',
+                paragraph: '#909090',
+                primary: {
+                    yellow: '#EACA29',
+                    orange: '#EA8035',
+                    DEFAULT: '#EACA29', // Default primary color
+                },
+            },
+            fontSize: {
+                // Custom heading sizes
+                h1: ['80px', { lineHeight: '1.1', fontWeight: '500' }],
+                h2: ['64px', { lineHeight: '1.1', fontWeight: '500' }],
+                h3: ['48px', { lineHeight: '1.2', fontWeight: '500' }],
+                h4: ['36px', { lineHeight: '1.2', fontWeight: '500' }],
+                h5: ['24px', { lineHeight: '1.3', fontWeight: '500' }],
+                h6: ['20px', { lineHeight: '1.3', fontWeight: '500' }],
+            },
+        },
     },
     plugins: [],
 }
